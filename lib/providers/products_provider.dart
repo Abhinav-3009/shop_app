25024780bc3dda5b691af5ef0ddad5b2fb345652
Abhinav Gupta <abhi30092000.ag@gmail.com>
2021-06-03 23:18:21 +0530

    added changeNotifierProvider widget in main.dart

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/Material.dart';
-
+import '../models/product.dart';
 class Products with ChangeNotifier{
-  
+  List<Product> _items=[];
+  List<Product> get items{
+    return [..._items];
+  }
+
+  void addProducts(){
+    notifyListeners();
+  }
 }
