0f8b9a4d98e58aeccf86fb40e4c9efffa0849618
Abhinav Gupta <abhi30092000.ag@gmail.com>
2021-07-14 22:32:18 +0530

    added side drawer

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
-import 'package:flutter/foundation.dart';
-
 enum FilterOptions {
   Favorites,
   All,
 }
-
 class ProductsOverviewScreen extends StatefulWidget {
   @override
   _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
@@ -61,6 +59,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
